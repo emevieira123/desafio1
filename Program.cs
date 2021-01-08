15a2a815d0e3eec8c35f2937e7dc1dd82cfbecf7
Emerson Vieira <emevieira@gmail.com>
2021-01-08 08:48:13 -0400

    Atualizacao do codigo

--- Program.cs
+++ Program.cs
@@ -4,49 +4,48 @@ namespace desafio1
 {
     class Program
     {
+
+
         static void Main(string[] args)
-        {
-            double valor1, valor2;
-            double maior, menor;
-            
-            Console.WriteLine("Digite o primeiro valor:");
-
-            valor1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo valor:");
-
-            valor2 = Convert.ToDouble(Console.ReadLine());            
-
-            if (valor1 >= valor2)
+        {            
+            double divide(double divisor, double dividendo)
             {
-                maior = valor1;
-
-                menor = valor2;
-            }
-            else
-            {
-                maior = valor2;
-
-                menor = valor1;
+                return divisor + dividendo;
             }
 
-            double divisao = maior;
-
+            double valor1 = 0;
+            double valor2 = 0;
             double resultado = 0;
+            double i = 0;
 
-            while (divisao > 0)
+            do
             {
-                divisao = divisao + menor;
+                Console.WriteLine("Digite o primeiro valor:");
+                valor1 = Convert.ToDouble(Console.ReadLine());
 
-                resultado++;
+                Console.WriteLine("Digite o primeiro valor:");
+                valor2 = Convert.ToDouble(Console.ReadLine());
+
+                resultado = divide(valor1, valor2);
+                i++;
+
+                while (resultado >= valor2)
+                {
+                    i++;
+                    resultado = divide(resultado, valor2);
+                }
+
+                System.Console.WriteLine("{0} dividido por {1} = {2}", valor1, valor2, i);
+
+                i = 0;
             }
-            
-            Console.WriteLine(string.Format("Resultado da divisão: {0}", resultado));            
-
-            Console.ReadKey();
+            while (valor1 != 0);
+            {
+                Console.ReadKey();
+            }
 
         }
-        
+
     }
 
 }
