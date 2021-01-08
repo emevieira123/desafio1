62e91c12bd8e5e1982de6c0e49280fd7d162626b
Emerson Vieira <emevieira@gmail.com>
2021-01-08 00:24:23 -0400

    Teste do calculo falhou

--- Program.cs
+++ Program.cs
@@ -6,7 +6,47 @@ namespace desafio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double valor1, valor2;
+            double maior, menor;
+            
+            Console.WriteLine("Digite o primeiro valor:");
+
+            valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+
+            valor2 = Convert.ToDouble(Console.ReadLine());            
+
+            if (valor1 >= valor2)
+            {
+                maior = valor1;
+
+                menor = valor2;
+            }
+            else
+            {
+                maior = valor2;
+
+                menor = valor1;
+            }
+
+            double divisao = maior;
+
+            double resultado = 0;
+
+            while (divisao > 0)
+            {
+                divisao = divisao + menor;
+
+                resultado++;
+            }
+            
+            Console.WriteLine(string.Format("Resultado da divisão: {0}", resultado));            
+
+            Console.ReadKey();
+
         }
+        
     }
+
 }
