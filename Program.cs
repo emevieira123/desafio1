64c030137599e4841dda49b06f15a0438ec0b162
Emerson Vieira <emevieira@gmail.com>
2021-01-09 17:16:08 -0400

    Atualizado calculo sem o valor decimal

--- Program.cs
+++ Program.cs
@@ -7,42 +7,85 @@ namespace desafio1
 
 
         static void Main(string[] args)
-        {            
-            double divide(double divisor, double dividendo)
-            {
-                return divisor + dividendo;
-            }
+        {
+            // double valor1, valor2;
+            // double maior, menor;
 
-            double valor1 = 0;
-            double valor2 = 0;
+            // Console.WriteLine("Digite o primeiro valor:");
+
+            // valor1 = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Digite o segundo valor:");
+
+            // valor2 = Convert.ToDouble(Console.ReadLine()); 
+
+
+            // //Faz a verificacao para ver qual variavel possui o maior valor
+            // if (valor1 >= valor2)
+            // {
+            //     maior = valor1;
+
+            //     menor = valor2;
+            // }
+            // else
+            // {
+            //     maior = valor2;
+
+            //     menor = valor1;
+            // }
+
+            // //Atribui o maior valor a variavel divisao
+            // double divisao = maior;
+
+            // double resultado = 0;
+
+            // while (divisao > 0)
+            // {
+            //     divisao = divisao + menor;
+
+            //     resultado++;
+            // }
+
+            // Console.WriteLine(string.Format("Resultado da divisão: {0}", resultado));            
+
+            // Console.ReadKey();
+
+            double divide(double divisor, double dividendo)
+            { return divisor + dividendo; }
+
+            double valor1 = 40;
+            double valor2 = 2;
             double resultado = 0;
             double i = 0;
+            //double j = 0;
+            // double t = 0;
+            double d = 0;                             
+                 
+                resultado = valor2;                              
 
-            do
-            {
-                Console.WriteLine("Digite o primeiro valor:");
-                valor1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Digite o primeiro valor:");
-                valor2 = Convert.ToDouble(Console.ReadLine());
-
-                resultado = divide(valor1, valor2);
-                i++;
-
-                while (resultado >= valor2)
+                while (resultado <= valor1)
                 {
                     i++;
                     resultado = divide(resultado, valor2);
                 }
 
-                System.Console.WriteLine("{0} dividido por {1} = {2}", valor1, valor2, i);
+                
 
-                i = 0;
-            }
-            while (valor1 != 0);
-            {
-                Console.ReadKey();
-            }
+                // t = 1;
+                // j = valor1 + valor1 + valor1;
+                // double resultado2 = t;
+
+                // while(resultado2 <= j)
+                // {
+                //     d++;
+                //     resultado2 = divide(resultado2, t);
+                // }
+
+                System.Console.WriteLine("{0} dividido por {1} = {2}.{3}", valor1, valor2, i, d);     
+
+                double a = 40; double b = 2; double c;
+                c = a / b;
+                System.Console.WriteLine(c);       
 
         }
 
