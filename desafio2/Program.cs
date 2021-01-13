c5fee4dd7e6ee7858d57a24e47459dca58e2c381
Emerson Vieira <emevieira@gmail.com>
2021-01-13 15:26:30 -0400

    Implementacao para valores negativos em Subtrai e Multiplica

--- desafio2/Program.cs
+++ desafio2/Program.cs
@@ -9,16 +9,26 @@ namespace desafio2
             Matematica matematica = new Matematica();
 
             matematica.Soma(10, 5);
-            matematica.Subtrai(40, 7);
-            // matematica.Mutiplica(9, 9);
-            matematica.Exponencia(9, 3);
-
             System.Console.WriteLine(matematica.Somar);
+
+            matematica.Subtrai(40, 7);
             System.Console.WriteLine(matematica.Subtrair);
+
+            int multiplicacao = matematica.Mutiplica(-9, 9);
+            System.Console.WriteLine(multiplicacao);
+
+            matematica.Exponencia(9, 2);
+            System.Console.WriteLine(matematica.Exponenciar);
+
+            // matematica.Mutiplica(9, 9);
             // System.Console.WriteLine(matematica.Multiplicar);
-            int multiplicacao = matematica.Mutiplica(9,9);
-            System.Console.WriteLine(multiplicacao); 
-            System.Console.WriteLine(matematica.Exponenciar);         
+
+            // int result = matematica.Exponencia(9, 3);
+            // System.Console.WriteLine(result);
+
+
+            int teste = -9 * (-9);
+            System.Console.WriteLine("Comparacao * = {0}", teste);
 
         }
     }
@@ -38,25 +48,20 @@ namespace desafio2
         public int Subtrai(int primeiroValor, int segundoValor)
         {
             int count = 0;
+            int valor = segundoValor;
+            int resultado = primeiroValor;
             if (primeiroValor > segundoValor)
             {
-                int resultado = segundoValor;
-
-                while (resultado < primeiroValor)
+                while (valor < primeiroValor)
                 {
-                    resultado = resultado + 1;
+                    valor = valor + 1;
                     count++;
                 }
             }
-            else
+            while (resultado < segundoValor)
             {
-                int resultado = primeiroValor;
-
-                while (resultado < segundoValor)
-                {
-                    resultado = resultado + 1;
-                    count++;
-                }
+                resultado = resultado + 1;
+                count--;
             }
             return this.Subtrair = count;
         }
@@ -64,19 +69,41 @@ namespace desafio2
         public int Mutiplica(int multiplicando, int multiplicador)
         {
             int multiplicacao = 0;
-            for (int i = 0; i < multiplicando; i++)
+            if (multiplicando < multiplicacao || multiplicador < multiplicacao)
             {
-                multiplicacao = multiplicacao + multiplicador;
+                for (int i = 0; i < multiplicando; i++)
+                {
+                    multiplicacao = multiplicacao + multiplicador;
+                }
             }
-            return multiplicacao;
+            // if(multiplicando < 0 && multiplicador < 0)
+            // {
+            //     for (int i = 0; i > multiplicando; i--)
+            //     {
+            //         multiplicacao = multiplicacao + multiplicador;
+            //     }
+            // }
+
+            for (int i = 0; i < multiplicador; i++)
+            {
+                multiplicacao = multiplicacao + multiplicando;
+            }
+            return this.Multiplicar = multiplicacao;
         }
 
         public int Exponencia(int numeroBase, int expoente)
         {
             int result = numeroBase;
+            if (numeroBase < 0)
+            {
+                for (int count1 = 0; count1 < expoente - 1; count1++)
+                {
+                    result = Mutiplica(result, numeroBase);                    
+                }
+            }
             for (int count1 = 0; count1 < expoente - 1; count1++)
             {
-                result = Mutiplica(result, numeroBase); 
+                result = Mutiplica(result, numeroBase);
                 // for (int i = 0; i < numeroBase; i++)
                 // {
                 //     result = result + expoente;
