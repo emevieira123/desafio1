40edb7085fabe9f3b17eabe848cacbc0a2d2841e
Emerson Vieira <emevieira@gmail.com>
2021-01-15 15:35:11 -0400

    Reformulando o codigo

--- desafio1/Program.cs
+++ desafio1/Program.cs
@@ -8,46 +8,28 @@ namespace desafio1
 
         static void Main(string[] args)
         {
-            Decimal divide(Decimal divisor, Decimal dividendo)
-            { return divisor + dividendo; }
-
             Decimal valor1 = 17; Decimal valor2 = 5;
-            Decimal i = 0; Decimal d = 0; Decimal count = 0; 
+            Decimal i = 0; Decimal d = 0;
             Decimal recebeDecimal = 0.1m;          
 
             Decimal resultado = valor2;
             while (resultado <= valor1)
             {
                 i++;
-                resultado = divide(resultado, valor2);
+                resultado = resultado +valor2;
             }
-
-            //Decimal resultado2 = valor2;
-            //Decimal resultado2 = valor2 + i;
+            
             Decimal resultado2 = resultado + resultado;
-            Decimal teste = valor2;
-            Decimal resultadoDecimal = recebeDecimal;
-
-            // for(d = 0; d < resultado2; d++)
-            // {
-            //     t++;
-            //     resultado2 = resultadoDecimal + resultadoDecimal;
-            // }
 
             while(valor2 <= resultado2)
             {
                 d++;
-                resultado2 = divide(teste, teste);                
+                resultado2 = valor2 + valor2;                
             }
 
             i = i + recebeDecimal;
 
-            System.Console.WriteLine(resultado2);
-            System.Console.WriteLine(count);
-            System.Console.WriteLine(d);
             System.Console.WriteLine("{0} dividido por {1} = {2:N2}", valor1, valor2, i);
-
-
 
             
             // Decimal dividendo = 17;
@@ -62,6 +44,28 @@ namespace desafio1
             // c = valor1 / valor2;
             // System.Console.WriteLine("Para comparacao: {0}", c);
 
+        }
+
+        public static Decimal Divide(Decimal dividendo, Decimal divisor)
+        {
+            Decimal i = 0;
+            Decimal quociente = divisor;
+
+            while(quociente <= dividendo)
+            {
+                i++;
+                quociente = quociente + divisor;
+            }
+
+            Decimal resto = 0;
+
+            //Fazer aqui o codigo para descobrir o resto;
+
+            Decimal novoDividendo = Multiplica(resto, 10);
+
+            var decimais = Divide(novoDividendo, divisor);
+
+            return 0.1m;
         }
 
     }
