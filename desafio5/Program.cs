e3edf9d65fe8864292c9e65722593dab200f27b3
Emerson Vieira <emevieira@gmail.com>
2021-01-16 00:55:06 -0400

    Implementacao do codigo do projeto Fibonacci

---
+++ desafio5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace desafio5
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            int ValorEntrada = 15;
+            Fibonacci fibonacci = new Fibonacci();
+
+            for(int i = 0; i < ValorEntrada; i++)
+            {
+                //Recebe o valor da variavel (valorAnterior) e faz a contagem atraves da variavel (i) a quantidade de vezes informada na variavel (ValorEntrada).
+                System.Console.WriteLine("{0}", fibonacci.Calculo(i)); 
+            }
+        }
+    }
+
+    public class Fibonacci
+    {
+        public int Calculo(int valor)
+        {
+            int valorAnterior = 0;
+            int valorAtual = 1;
+            int proximoValor = 0;                       
+
+            for(int i = 0; i < valor; i++) 
+            {   //Formula onde é realizado o calculo conforme a sequencia Fibonacci
+                proximoValor = valorAnterior;
+			    valorAnterior = valorAtual;
+			    valorAtual = proximoValor + valorAtual;
+                		                
+            }
+            return valorAnterior;            
+            
+        }
+    }
+}
