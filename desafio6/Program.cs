2d20b96901ca218f1842f248776f8b9ab555e521
Emerson Vieira <emevieira@gmail.com>
2021-01-21 09:30:31 -0400

    Criacao do projeto de adivinhacao

---
+++ desafio6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace desafio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            String iniciar = "i";     
+            String maior = ">";
+            String menor = "<";
+            String acertou = "=";
+            int count = 0;
+            int teste;
+            int[] array = new int[100];
+
+            System.Console.WriteLine("Pense em um numero de 0 a 100");
+
+            teste = Random();
+        }
+    }
+}
