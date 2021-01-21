d2db77c1d4a976688e05923c05d266efb21603c6
Emerson Vieira <emevieira@gmail.com>
2021-01-21 09:33:30 -0400

    Implementacao do cogido que soma valores de um array

---
+++ desafio7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace desafio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Array array = new Array();
+            System.Console.WriteLine(array.SomaDoArray());
+        }
+    }
+    public class Array
+    {
+        public int SomaDoArray()
+        {
+            int totalSoma = 0;
+            int[] arraySoma = new int[]{3, 5, 7, 9, 11};
+            
+            totalSoma = arraySoma[0] + arraySoma[1] + arraySoma[2] + arraySoma[3] + arraySoma[4];
+
+            return totalSoma;
+        }
+    }
+}
