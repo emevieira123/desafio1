9765cddb15e9414d800d75c719e58adc65fa32ba
Emerson Vieira <emevieira@gmail.com>
2021-01-22 09:17:23 -0400

    Implementacao do codigo do jogo de adivinhacao

--- desafio6/Program.cs
+++ desafio6/Program.cs
@@ -5,18 +5,36 @@ namespace desafio6
     class Program
     {
         static void Main(string[] args)
-        {       
-            String iniciar = "i";     
-            String maior = ">";
-            String menor = "<";
-            String acertou = "=";
-            int count = 0;
-            int teste;
-            int[] array = new int[100];
+        {
+            int[] deUMaCEM = new int[]{1, 100};
+            int numeroPalpite = 0;
+            int numeroInicial = deUMaCEM[0];
+            int numeroFinal = deUMaCEM[1];
 
-            System.Console.WriteLine("Pense em um numero de 0 a 100");
+            System.Console.WriteLine("PENSE EM UM NÚMERO DE {0} A {1}.", numeroInicial, numeroFinal);
+            System.Console.WriteLine("Informe com '<' ou '>' se o numero for maior ou menor");
+            System.Console.WriteLine("Quando acertar o numero pensado digite '=' ");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Tecle Enter para iniciar!");
+            System.Console.ReadLine();
 
-            teste = Random();
+            for (int i = 1; i < 10; i++)
+            {
+                System.Console.WriteLine("Tentativa {0}", i);
+                numeroPalpite = (numeroInicial + numeroFinal) / 2;
+
+                System.Console.WriteLine("O número pensado é {0}?", numeroPalpite);
+                string recebeSinal = System.Console.ReadLine();
+
+                if (recebeSinal == ">")
+                {   
+                    numeroInicial = numeroPalpite;
+                    System.Console.WriteLine(numeroPalpite);
+                }
+                else if (recebeSinal == "<") { numeroFinal = numeroPalpite; }
+                else if (recebeSinal == "=") { System.Console.WriteLine("Até que enfim eu acertei kkk"); break; }
+                if (i == 9) { System.Console.WriteLine("Voce excedeu seu numero de tentativas"); break; }
+            }
         }
     }
 }
