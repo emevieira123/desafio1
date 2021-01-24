4e6671a95b0a189567c7e43afd5a8b2db99418b7
Emerson Vieira <emevieira@gmail.com>
2021-01-24 16:58:33 -0400

    Implementacao do codigo do desafio 8

--- desafio8/Program.cs
+++ desafio8/Program.cs
@@ -5,8 +5,25 @@ namespace desafio8
     class Program
     {
         static void Main(string[] args)
+        {                       
+            System.Console.WriteLine(quadradoMaior(12, 30));
+        }
+        public static int quadradoMaior(int largura, int comprimento)
         {
-            Console.WriteLine("Hello World!");
+            int result = 0, i = 0;
+            
+            while(largura < comprimento)
+            {
+                i++;
+                comprimento = comprimento - largura;                              
+            }
+            result = comprimento;
+            while(result < largura)
+            {
+                i++;
+                largura = largura - result;
+            }
+            return largura;            
         }
     }
 }
